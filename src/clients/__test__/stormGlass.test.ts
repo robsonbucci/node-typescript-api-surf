585510aa655e9afe8fe217bd5b0516bc1d8040f8
robsonbucci <robsonbucci@hotmail.com>
2024-06-29 19:48:40 -0300

    treat rate limit reached error

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -50,4 +50,22 @@ describe('StormGlass client', () => {
       'Unexpected error when trying to communicate to StormGlass: Network Error'
     );
   });
+
+  it('should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
+    const lat = -33.432879;
+    const lng = 132.634564;
+
+    mockedAxios.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { errors: ['Rate Limit reached'] },
+      },
+    });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
+    );
+  });
 });
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -39,6 +39,14 @@ export class ClientRequestError extends InternalError {
   }
 }
 
+export class StormGlassResponseError extends InternalError {
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error returned by the StormGlass service';
+    super(`${internalMessage}: ${message}`);
+  }
+}
+
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
@@ -61,6 +69,10 @@ export class StormGlass {
       );
       return this.normalizeResponse(response.data);
     } catch (error: any) {
+      if (error.response && error.response.status)
+        throw new StormGlassResponseError(
+          `Error: ${JSON.stringify(error.response.data)} Code: ${error.response.status}`
+        );
       throw new ClientRequestError(error.message);
     }
   }
