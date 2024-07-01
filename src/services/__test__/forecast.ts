6dbb7b709d9118b0a39e48cc01be274a1dad4d44
robsonbucci <robsonbucci@hotmail.com>
2024-07-01 18:06:36 -0300

    create a list of beaches

---
+++ src/services/__test__/forecast.ts
@@ -0,0 +1,46 @@
+import { IForecastPoint, StormGlass } from '@src/clients/stormGlass';
+
+export enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
+}
+
+export interface IBeach {
+  name: string;
+  position: BeachPosition;
+  lat: number;
+  lng: number;
+  user: string;
+}
+
+export interface IBeachForecast extends Omit<IBeach, 'user'>, IForecastPoint {}
+
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+
+  public async processForecastForBeaches(
+    beaches: IBeach[]
+  ): Promise<IBeachForecast[]> {
+    const pointsWithCorrectSources: IBeachForecast[] = [];
+
+    for (const beach of beaches) {
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = points.map((e) => ({
+        ...{
+          lat: beach.lat,
+          lng: beach.lng,
+          name: beach.name,
+          position: beach.position,
+          rating: 1,
+        },
+        ...e,
+      }));
+
+      pointsWithCorrectSources.push(...enrichedBeachData);
+    }
+
+    return pointsWithCorrectSources;
+  }
+}
