50e9768818c4318998bb14a8b0accce6c1c1eac0
robsonbucci <robsonbucci@hotmail.com>
2024-07-12 17:13:58 -0300

    add test to beaches module

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import './util/module-alias';
 import bodyParser from 'body-parser';
 import { Server } from '@overnightjs/core';
 import { ForecastController } from './controllers/forecast';
+import { BeachesController } from './controllers/beaches';
 import { Application } from 'express';
 import * as database from '@src/database';
 
@@ -22,7 +23,8 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
 
   private async databaseSetup(): Promise<void> {
